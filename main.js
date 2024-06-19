2d47fc983b1057a0845c12b9cd02ade516df574b
Axel147 <lopez.b.axel@gmail.com>
2024-06-18 21:18:16 -0300

    Armado de Electron
    Se instalo Electro y se configuro para su funcionamiento

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+//app => controla el ciclo de vida del evento de la aplicacion
+//BrowserWindow => crea y administra ventanas de aplicaciones.
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('src/app/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
